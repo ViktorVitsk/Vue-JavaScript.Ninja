a9f578ed72df7f7f1e7e95e730f76a57e552f00a
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-01-23 23:10:42 +0200

    feat: add saving to localStorage

--- cryptonomicon/src/App.vue
+++ cryptonomicon/src/App.vue
@@ -175,16 +175,30 @@ export default {
         name: this.ticker,
         price: "-",
       };
+
       this.tickers.push(currentTicker);
+
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+
+      this.subscribeToUpdates(currentTicker.name);
+    },
+
+    subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=${this.API}`
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=${this.API}`
         );
         const data = await f.json();
-        this.tickers.find((t) => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
 
-        if (this.sel?.name === currentTicker.name) {
+        try {
+          if (typeof data.USD === "number")
+            this.tickers.find((t) => t.name === tickerName).price =
+              data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        } catch (e) {
+          console.log("бля");
+        }
+
+        if (this.sel?.name === tickerName) {
           this.graph.push(data.USD);
         }
       }, 3000);
@@ -192,9 +206,9 @@ export default {
       this.coinSearch = [];
       this.coinContain = false;
     },
-
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
     },
 
     normalizeGraph() {
@@ -240,8 +254,16 @@ export default {
       this.add();
     },
   },
-  async created() {
-    await this.getCoinList();
+  created() {
+    this.getCoinList();
+
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
   },
 };
 </script>
